9e7577bb271fe6ac15eb87243d3562d9c5c9dad1
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-05-08 21:43:40 +0100

    add format of the number. track-1-part-4

--- exercise1/add.go
+++ exercise1/add.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 func main() {
-	result := Add(1, 2, 3, 4.2)
-	fmt.Println(result)
+	result := Add(1209, 234562, 7658493)
+
+	fmt.Println(formatNumber(result))
 }
 
-func Add(numbers ...interface{}) interface{} {
+func Add(numbers ...interface{}) int {
 	sum := 0
 	for _, num := range numbers {
 		if i, ok := num.(int); ok {
@@ -17,4 +20,18 @@ func Add(numbers ...interface{}) interface{} {
 		}
 	}
 	return sum
+}
+
+func formatNumber(num int) string {
+	if num > 9999 {
+		str := fmt.Sprintf("%d", num)
+		re := regexp.MustCompile("(\\d+)(\\d{3})")
+
+		for n := ""; n != str; {
+			n = str
+			str = re.ReplaceAllString(str, "$1,$2")
+		}
+		return str
+	}
+	return strconv.Itoa(num)
 }
