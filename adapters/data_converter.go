5f731d35442e3f83d34c8b9a4a8f9377698dd971
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-05-12 20:39:51 +0100

    trigger pipeline to fix new git set up

--- adapters/data_converter.go
+++ adapters/data_converter.go
@@ -1,23 +1,16 @@
 package adapters
 
 import (
-	"fmt"
 	"regexp"
 	"strconv"
 )
 
 func DataConverter(input string) []int {
-	// we get an input which is a string with numbers, commas and new lines
-	fmt.Printf("input %q\n", input)
-
-	//variables
 	var arrayOfnumbers []string
 	var numbersFromFile []int
 
-	//get rid of commas and new lines to get array of strings
 	a := regexp.MustCompile(`(\s*(,|\n)\s*)`)
 	arrayOfnumbers = a.Split(input, -1)
-	fmt.Printf("array of strings that are numbers %q\n", arrayOfnumbers)
 
 	for _, number := range arrayOfnumbers {
 		integer, _ := strconv.Atoi(number)
