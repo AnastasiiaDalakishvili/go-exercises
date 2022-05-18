01a99b6e534570a04deb0d67f7d9d0bc72bd1851
AnastasiiaDalakishvili <anastasiia.dalakishvili@saltpay.co>
2022-05-18 20:07:28 +0100

    refactor calculator to only calculate the sum of numbers it receives

--- cmd/main.go
+++ cmd/main.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main() {
+	//extract the numbers pass by user either from the file or just an array of integers
+	numbers := adapters.GetNumberFromFile()
 
-	sum := domain.Add()
+	//pass this numbers ro calculator and get the sum
+	sum := domain.Add(numbers)
+
+	//pass sum to formatter and get desired formatted sum
 	formattedSum := domain.FormatNumber(sum)
+
+	//print sum
 	adapters.Printer(formattedSum)
 }
--- domain/calculator.go
+++ domain/calculator.go
@@ -1,21 +1,25 @@
 package domain
 
-import "github.com/AnastasiiaDalakishvili/go-exercise/adapters"
-
-func Add(numbersPassedtoFunction ...interface{}) int {
+func Add(numbers []int) int {
 	sum := 0
-	if numbersPassedtoFunction != nil {
-		//calculate sum
-		for _, num := range numbersPassedtoFunction {
-			if i, ok := num.(int); ok {
-				sum += i
-			}
-		}
-	} else {
-		numbersFromFile := adapters.GetNumberFromFile()
-		for _, num := range numbersFromFile {
-			sum += num
-		}
+	for _, num := range numbers {
+		sum += num
 	}
 	return sum
 }
+
+//sum := 0
+//if numbersPassedtoFunction != nil {
+////calculate sum
+//for _, num := range numbersPassedtoFunction {
+//if i, ok := num.(int); ok {
+//sum += i
+//}
+//}
+//} else {
+//numbersFromFile := adapters.GetNumberFromFile()
+//for _, num := range numbersFromFile {
+//sum += num
+//}
+//}
+//return sum
